2eb98dff31c49d9629fcb2f89181c34f033ab173
diixo <anatolii.podoba@gmail.com>
2025-07-25 00:32:12 +0300

    updated training product result

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 
-import pandas as pd
+
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments
 from datasets import Dataset, DatasetDict, load_dataset
-import re
+from dataclasses import dataclass
+from typing import Any, Dict, List
+import torch
+
 
 IGNORE_INDEX = -100
 
+##################################################################
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+MODEL_PATH = "./gpt2-dialog"
+
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-tokenizer.pad_token = tokenizer.eos_token
-
-def check_special_tokens():
-    print(f"eos_token: {tokenizer.eos_token}={tokenizer.eos_token_id}, decode={tokenizer.decode([tokenizer.eos_token_id])}")
-    print(f"pad_token_id: {tokenizer.pad_token}={tokenizer.pad_token_id}, decode={tokenizer.decode([tokenizer.pad_token_id])}")
-#check_special_tokens()
-
-model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 num_added_toks = tokenizer.add_special_tokens({
     'additional_special_tokens': ["<|im_start|>", "<|im_end|>"],
     'pad_token': "<|pad|>"
     })
+tokenizer.pad_token = "<|pad|>"
+
+tokenizer.save_pretrained(MODEL_PATH)
+##################################################################
+
+def check_special_tokens(tokenizer: GPT2Tokenizer):
+    print(f"eos_token: {tokenizer.eos_token}={tokenizer.eos_token_id}, decode={tokenizer.decode([tokenizer.eos_token_id])}")
+    print(f"pad_token_id: {tokenizer.pad_token}={tokenizer.pad_token_id}, decode={tokenizer.decode([tokenizer.pad_token_id])}")
+check_special_tokens(tokenizer)
+
+model = GPT2LMHeadModel.from_pretrained('gpt2').to(DEVICE)
+
 model.resize_token_embeddings(len(tokenizer), mean_resizing=False)
 
 dataset = load_dataset("json",
-        data_files="./data/test_debug.json",
-        split="train")
+    data_files=["./data/driver_mood_focus_100.json",
+                "./data/driver_supermarket_100_dialogs.json",
+                "./data/driver_work_100_varied_dialogs.json",
+                "./data/intent_training_dialogues.json",
+                "./data/psych_state_100_dialogs.json",
+                "./data/test_debug.json",
+                "./data/uncertainty_dialogs_100.json",
+                "./data/uncertainty_film_final_100.json",
+                "./data/uncertainty_film_more_100.json",
+                ],
+    split="train")
 
 print(f"dataset.sz={len(dataset)}")
 
@@ -84,10 +104,6 @@ def preprocess_fn(example):
 processed_dataset = dataset.map(preprocess_fn, remove_columns=dataset.column_names)
 
 
-from dataclasses import dataclass
-from typing import Any, Dict, List
-import torch
-
 @dataclass
 class DataCollatorForCausalLMwithIgnorePad:
     tokenizer: Any
@@ -120,10 +136,10 @@ collator = DataCollatorForCausalLMwithIgnorePad(tokenizer=tokenizer, ignore_inde
 
 training_args = TrainingArguments(
     output_dir="./train_products",
-    per_device_train_batch_size=2,
-    num_train_epochs=1,
+    per_device_train_batch_size=8,
+    num_train_epochs=3,
     learning_rate=3e-5,
-    logging_steps=1,
+    logging_steps=5,
     save_strategy="no",
     save_total_limit=1,
     lr_scheduler_type="constant",
@@ -137,3 +153,5 @@ trainer = Trainer(
 )
 
 trainer.train()
+
+trainer.model.save_pretrained(MODEL_PATH)
